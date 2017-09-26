df6129530c95f98c961888352219aa969c4d8242
Luka Zdravkovic <lukazd99@outlook.com>
2017-09-26 21:38:11 +0200

    Web servis
    Dodat ugovor servisa i njegova implementacija (1 klasa, 1 interfejs)

---
+++ ITVRMService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVRMHost
+{
+    [ServiceContract]
+    interface ITVRMService
+    {
+        [OperationContract]
+        bool ShortClasses(bool check);
+        [OperationContract]
+        string DisplayMessage(string message, DateTime startDate, DateTime endDate);
+    }
+}
---
+++ TVRMService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVRMHost
+{
+    class TVRMService : ITVRMService
+    {
+        public string DisplayMessage(string message, DateTime startDate, DateTime endDate)
+        {
+            return (startDate == null) ? message + ";" + DateTime.Now.ToString() + ";" + endDate.ToString() :
+                message + ";" + startDate.ToString() + ";" + endDate.ToString();
+        }
+
+        public bool ShortClasses(bool check)
+        {
+            return check;
+        }
+    }
+}
